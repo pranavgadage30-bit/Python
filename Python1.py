4acf5b111866c2e99338b86706dcc6c15ca43bfe
Pranav Gadage <pranavgadage30@gmail.com>
2026-04-29 20:34:59 +0530

    Add files via upload

---
+++ Python1.py
@@ -0,0 +1,52 @@
+print("Hello World")
+print("how r u?")
+ # Line Break 
+print("Darshan Patil\n ADYPU\n 2027")
+
+#Types
+
+a = 10 
+print(a)
+print(type(a))
+
+b = 10.10 
+print(b)
+print(type(b))
+
+ch = 'z' 
+print(ch)
+print(type(ch))
+
+name = "ram"
+print(name)
+print(type(name))
+
+# Comparison / Relational : <>,<=,>=,==,!=
+
+a = 20
+b = 15
+
+print(a < b)
+print(a > b)
+print(a <= b)
+print(a >= b)
+print(a == b)
+print(a != b)
+
+# Assignment Operators : +=, -=, %=, *= 
+b = 4
+b -=2 
+print(b)
+
+y = 20
+y /=10
+print(y)
+
+
+# Logical Operators - $$, ||, ! 
+a = 10
+b = 20
+print(a>b and b<a)
+print(a>b or b<a)
+print(b>a and a<b or a==b)
+
